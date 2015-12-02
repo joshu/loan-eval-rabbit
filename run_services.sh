717bb0f4b4953e9a3da9ce1c9fd71877788afe02
Jim Holt <jim_holt@freddiemac.com>
2015-12-02 12:14:07 -0500

    shell script updates

--- run_services.sh
+++ run_services.sh
@@ -1,6 +1,6 @@
-#! /usr/local/bin/bash
-docker run -d -e RABBIT_ADDR='172.17.0.2' --name aggregator fre/aggregator
-docker run -d -e RABBIT_ADDR='172.17.0.2' --name credit fre/credit
-docker run -d -e RABBIT_ADDR='172.17.0.2' --name gateway fre/gateway
-docker run -d -e RABBIT_ADDR='172.17.0.2' --name purchase fre/purchase
-docker run -d -e RABBIT_ADDR='172.17.0.2' --name validate fre/validate
+#! /usr/bin/bash
+docker run -d -e RABBIT_ADDR='172.17.0.3' --name aggregator fre/aggregator
+docker run -d -e RABBIT_ADDR='172.17.0.3' --name credit fre/credit
+docker run -d -e RABBIT_ADDR='172.17.0.3' --name gateway fre/gateway
+docker run -d -e RABBIT_ADDR='172.17.0.3' --name purchase fre/purchase
+docker run -d -e RABBIT_ADDR='172.17.0.3' --name validate fre/validate
